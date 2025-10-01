0e9d19ee27556cc45a3936caab480b92db8e985a
carlosrs14 <crincones578@gmail.com>
2025-09-30 20:05:10 -0500

    rest template on consumer client done

---
+++ microservice-client-old/src/main/java/com/bloque3/microservice_client_old/dtos/request/ProductRequestDTO.java
@@ -0,0 +1,19 @@
+package com.bloque3.microservice_client_old.dtos.request;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductRequestDTO {
+    private String name;
+
+    private String description;
+    
+    private Double price;
+    
+    private Integer stock;
+}
