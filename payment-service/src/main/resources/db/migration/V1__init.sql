b6db4a09ebe9b979e42748fa566b1b78a280d67a
carlosrs14 <crincones578@gmail.com>
2025-11-14 14:02:04 -0500

    solving script's lexical errors

--- payment-service/src/main/resources/db/migration/V1__init.sql
+++ payment-service/src/main/resources/db/migration/V1__init.sql
@@ -1,7 +1,7 @@
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 CREATE TABLE IF NOT EXISTS payments (
-    id UUID PRIMARY KEY gen_random_uuid(),
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     order_id UUID NOT NULL,
-    amount NUMERIC(10,2) NOT NULL,
-)
+    amount NUMERIC(10,2) NOT NULL
+);
