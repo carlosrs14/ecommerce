3146755980dd1c51c190b1ea64d3d897bb8f45e4
carlosrs14 <crincones578@gmail.com>
2025-11-14 13:45:41 -0500

    add dependencies and docker config

--- product-service/src/main/resources/db/migration/V1_init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS  products (
-    id UUID PRIMARY KEY gen_random_uuid(),
-    name VARCHAR(100) NOT NULL,
-    description VARCHAR(255),
-    stock INT NOT NULL,
-    price NUMERIC(10,2) NOT NULL,
-    active BOOLEAN NOT NULL
-);
